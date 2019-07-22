b4e551509cbae8d3bbe3548ba751ce1f10887d59
Denis Voloshin <denvol1996@gmail.com>
2019-07-22 23:07:08 +0300

    Optimized objectDiff function

--- utils/object/object-diff.js
+++ utils/object/object-diff.js
@@ -1,39 +1,17 @@
 import * as R from 'ramda';
 
-const isObject = R.compose(R.equals('Object'), R.type);
-const allAreObjects = R.compose(R.all(isObject), R.values);
-const hasLeft = R.has('left');
-const hasRight = R.has('right');
-const hasBoth = R.both(hasLeft, hasRight);
-const isEqual = R.both(hasBoth, R.compose(R.apply(R.equals), R.values));
+const _diff = (base, object) => {
+  const diff = {};
 
-const markAdded = R.compose(R.append(void 0), R.values);
-const markRemoved = R.compose(R.prepend(void 0), R.values);
-const isAddition = R.both(hasLeft, R.complement(hasRight));
-const isRemoval = R.both(R.complement(hasLeft), hasRight);
+  R.forEachObjIndexed((value, key) => {
+    if (!R.equals(value, base[key])) {
+      diff[key] = R.type(value) === 'Object' && R.type(base[key]) === 'Object'
+        ? _diff(base[key], value)
+        : value;
+    }
+  })(object);
+
+  return diff;
+};
 
 export const objectDiff = R.curry(_diff);
-
-function _diff(l, r) {
-  return R.compose(
-    R.map(
-      R.cond([
-        [isAddition, markAdded],
-        [isRemoval, markRemoved],
-        [
-          hasBoth,
-          R.ifElse(
-            allAreObjects,
-            R.compose(R.apply(objectDiff), R.values),
-            R.values
-          )
-        ]
-      ])
-    ),
-    R.reject(isEqual),
-    R.useWith(
-      R.mergeWith(R.mergeRight),
-      [R.map(R.objOf('left')), R.map(R.objOf('right'))]
-    )
-  )(l, r);
-}
